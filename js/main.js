65c50d88c77df5b746a7852c55fd50589c406150
banh0006 <banh0006@algonquinlive.com>
2019-04-26 12:53:02 -0400

    change when user login

--- js/main.js
+++ js/main.js
@@ -250,8 +250,8 @@ let web = {
         location.href = web.WEBURL + "admin/pizzas.html"
       } else {
         location.href = web.WEBURL + "profile.html";
-        document.querySelector('.pizza-admin-link').style.display = 'none';
-        document.querySelector('.ingredient-admin-link').style.display = 'none';
+        document.querySelector('.pizza-admin-link').setAttribute("hidden", true);
+        document.querySelector('.ingredient-admin-link').setAttribute("hidden", true);
       }
 
     }).catch(err => {
